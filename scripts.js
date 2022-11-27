f392f6a050f0c3335b6f0234abf269f174afce16
kevin-hannegan <khannegan@gmail.com>
2022-11-27 13:04:34 -0800

    simplify searchbar

--- scripts.js
+++ scripts.js
@@ -1,22 +1,18 @@
-let recipes = [];
-let searchbar = null;
-
-const filterRecipe = async (element, searchText) => {
-    if (element.textContent.toLowerCase().includes(searchText)) {
-        element.style.display = null;
-    } else {
-        element.style.display = "none";
-    }       
-};
-const search = () => {
-        let searchText = document.getElementById("searchbar").value.toLowerCase();
-        let promises = recipes.map( (cur) => filterRecipe(cur, searchText));
-        Promise.all(promises);
-};
-
 const init = () => {
-    searchbar = document.getElementById("searchbar");
-    recipes = Array.from(document.querySelectorAll("li"));
+    const searchbar = document.getElementById("searchbar");
+    const recipes = Array.from(document.querySelectorAll("li"));
+    const filterRecipe = async (element, searchText) => {
+        if (element.textContent.toLowerCase().includes(searchText)) {
+            element.style.display = null;
+        } else {
+            element.style.display = "none";
+        }       
+    };
+    const search = () => {
+        const searchText = document.getElementById("searchbar").value.toLowerCase();
+        const promises = recipes.map( (cur) => filterRecipe(cur, searchText));
+        Promise.all(promises);
+    };
     searchbar.addEventListener("search", search);
 };
 window.onload = init;
